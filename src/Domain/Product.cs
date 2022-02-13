08f2868a26bcb4361ea1f06b77db97bd2f7c3ae1
Bryan <bryanlima200@outlook.com>
2022-02-13 16:34:26 -0300

    Adicionado entidades

---
+++ src/Domain/Product.cs
@@ -0,0 +1,13 @@
+﻿using EFCore.Multitenant.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCore.Multitenant.Domain
+{
+    public class Product : BaseEntity
+    {
+        public string Description { get; set; }
+    }
+}
